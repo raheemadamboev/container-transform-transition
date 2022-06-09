7dba6ad478923374aff92d066f399c703b1a83da
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-09 15:40:39 +0500

    setup injection

---
+++ app/src/main/java/xyz/teamgravity/containertransformtransition/injection/App.kt
@@ -0,0 +1,7 @@
+package xyz.teamgravity.containertransformtransition.injection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
