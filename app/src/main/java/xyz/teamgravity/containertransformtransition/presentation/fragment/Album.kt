320dd5ea5c955d6680a5464a67a534ce7e0bc808
raheemadamboev <vividricemaryandmax@gmail.com>
2022-06-09 18:57:40 +0500

    album

--- app/src/main/java/xyz/teamgravity/containertransformtransition/presentation/fragment/Album.kt
+++ app/src/main/java/xyz/teamgravity/containertransformtransition/presentation/fragment/Album.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import xyz.teamgravity.containertransformtransition.databinding.FragmentAlbumBinding
 
 class Album : Fragment() {
@@ -12,9 +14,37 @@ class Album : Fragment() {
     private var _binding: FragmentAlbumBinding? = null
     private val binding get() = _binding!!
 
+    private val args by navArgs<AlbumArgs>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         _binding = FragmentAlbumBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        updateUI()
+        button()
+    }
+
+    private fun updateUI() {
+        binding.apply {
+            toolbar.title = args.album.artist
+            imageI.setImageResource(args.album.cover)
+            artistT.text = args.album.artist
+            albumT.text = args.album.album
+        }
+    }
+
+    private fun button() {
+        onBack()
+    }
+
+    private fun onBack() {
+        binding.toolbar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
     override fun onDestroyView() {
